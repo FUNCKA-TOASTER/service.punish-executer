5b800a1e2bdef5d321d4c32103709cbf54fce44d
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-04-29 16:31:18 +1000

    .\producer\

---
+++ producer/__init__.py
@@ -0,0 +1,10 @@
+"""Module "producer".
+About:
+    Provides a producer client that sends data
+    to rabbit.queue-broker service. Using JSON.
+"""
+
+from .custom import producer
+
+
+__all__ = ("producer",)
